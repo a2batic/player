b56d07f5f110e5e0ce6001bf0de2dc20788affa8
Kanika Murarka <a2batic@localhost.localdomain>
2020-09-20 02:17:53 +0530

    Fixes HTML structure to work with css

--- player.js
+++ player.js
@@ -1,8 +1,27 @@
 $.ajax({
     url: "https://guidedlearning.oracle.com/player/latest/api/scenario/get/v_IlPvRLRWObwLnV5sTOaw/5szm2kaj/?callback=__5szm2kaj&amp;refresh=true&amp;env=dev&amp;type=startPanel&amp;vars%5Btype%5D=startPanel&amp;sid=none&amp;_=1582203987867",
     dataType: "jsonp",
+    crossOrigin: true,
     // Work with the response
     success: function( response ) {
-        console.log( response ); // server response
+        var tooltip = response.data.tiplates.tip;
+        var hoverTip = response.data.tiplates.hoverTip;
+        var nextButon=$('.next-btn');
+        var arr = response.data.structure.steps;
+        var mainDiv = $('<div></div>');
+        mainDiv.addClass('sttip');
+        mainDiv.attr('id', 'main-player');
+        var arr = response.data.structure.steps.map((step) => {
+            return step.id;
+        })
+
+         mainDiv.append(tooltip);
+         mainDiv.append(hoverTip);
+        
+         $('body').append(mainDiv);
+         $('[aria-label="Steps"]').addClass('tooltip');
+         $('[aria-label="Hover Tip"]').addClass('tooltip');       
     }
 });
+
+
