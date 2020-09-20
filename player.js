d1c1eb7d69d33953be2c9cefc309cb950a6a912a
Kanika Murarka <a2batic@localhost.localdomain>
2020-09-21 01:46:37 +0530

    Add funtion for back button

--- player.js
+++ player.js
@@ -3,37 +3,37 @@ $.ajax({
     dataType: "jsonp",
     crossOrigin: true,
     // Work with the response
-    success: function( response ) {
+    success: function (response) {
         var tooltip = response.data.tiplates.tip;
         var hoverTip = response.data.tiplates.hoverTip;
         var steps = response.data.structure.steps;
         var cssStyle = $('<style>' + response.data.css + '</style>');
         $('head').append(cssStyle);
-        
+
         var arr = response.data.structure.steps;
         var mainDiv = $('<div></div>');
 
-        
+
         mainDiv.addClass('sttip');
         mainDiv.attr('id', 'main-player');
         var arr = response.data.structure.steps.map((step) => {
             return step.id;
         })
 
-         mainDiv.append(tooltip);
-         //mainDiv.append(hoverTip);
-        
-         $('body').append(mainDiv);
+        mainDiv.append(tooltip);
+        //mainDiv.append(hoverTip);
 
-         //append Google page to main div;
+        $('body').append(mainDiv);
+
+        //append Google page to main div;
         var getGooglePage = document.getElementById('viewport');
         getGooglePage.parentNode.removeChild(getGooglePage);
         $('[data-iridize-id="content"]').append(getGooglePage);
 
         $('body').css('padding', '0');
-         $('body').css('margin', '0');
-         $('[aria-label="Steps"]').addClass('tooltip');
-         $('[aria-label="Hover Tip"]').addClass('tooltip');  
+        $('body').css('margin', '0');
+        $('[aria-label="Steps"]').addClass('tooltip');
+        $('[aria-label="Hover Tip"]').addClass('tooltip');
 
         //add footer to end of page
         $('.stFooter').css('bottom', '0');
@@ -44,31 +44,45 @@ $.ajax({
         $('.tooltip').css('height', '100%');
 
         //add function to next button
-        var nextButon=$('.next-btn');
+        var nextButon = $('.next-btn');
         var contentDiv = $('<div class="step-content"></div>');
         var stepSpan = $('<span id="0" class="step-span"></span>');
         stepSpan.append(steps[0].action.contents['#content']);
         contentDiv.append(stepSpan);
         $('.popover-content').append(contentDiv);
-        nextButon.click(function() {
+        nextButon.click(function () {
             var current = Number(($('.step-span').attr('id').replace('#', '')));
-            if(steps[current+1]){
-            var nextStep = steps[current+1];
-            contentDiv.empty();
-            stepSpan = $('<span id="'+current+1+'" class="step-span"></span>');
-            if(nextStep.followers && nextStep.followers[0] && nextStep.followers[0].condition){
-                stepSpan.append(nextStep.action.contents['#content']);
+            var newN = current + 1;
+            if (steps[newN]) {
+                var nextStep = steps[newN];
+                contentDiv.empty();
+                stepSpan = $('<span id="' + newN + '" class="step-span"></span>');
+                if (nextStep.followers && nextStep.followers[0] && nextStep.followers[0].condition) {
+                    stepSpan.append(nextStep.action.contents['#content']);
+                } else {
+                    stepSpan.append('');
+                }
+                contentDiv.append(stepSpan);
             } else {
-                stepSpan.append('');
+                alert('Tour is completed');
             }
-            
-            contentDiv.append(stepSpan);
-        } else {
-            alert('Tour is completed');
-        }
-          
-          });              
+
+        });
+
+        //add function to back button
+        var prevButon = $('.default-prev-btn');
+        prevButon.click(function () {
+            var current = Number(($('.step-span').attr('id').replace('#', '')));
+            var newC = current - 1;
+            if (steps[newC]) {
+                var prevStep = steps[newC];
+                contentDiv.empty();
+                stepSpan = $('<span id="' + newC + '" class="step-span"></span>');
+                stepSpan.append(prevStep.action.contents['#content']);
+                contentDiv.append(stepSpan);
+            } else {
+                alert('Start the tour');
+            }
+        });
     }
 });
-
-
